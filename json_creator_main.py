fae2a5697101ab1b4775708851476fc93e897d13
Sifat Ahmed <Sifat-Ahmed@users.noreply.github.com>
2021-05-21 11:58:57 +0900

    Update json_creator_main.py

--- json_creator_main.py
+++ json_creator_main.py
@@ -16,6 +16,12 @@ def parse_args():
                         type=str,
                         default='dataset_train')
 
+    parser.add_argument('--num-train-classes',
+                        help='Number of classes to take for training',
+                        required=False,
+                        type=str,
+                        default=30)
+
     parser.add_argument('--val-dir',
                         help='Validation image directory',
                         required=False,
@@ -26,6 +32,12 @@ def parse_args():
                         required=False,
                         type=str,
                         default='dataset_val')
+
+    parser.add_argument('--num-val-classes',
+                        help='Number of classes to take validation',
+                        required=False,
+                        type=str,
+                        default=5)
 
     parser.add_argument('--test-dir',
                         help='Test Image directory',
@@ -38,6 +50,12 @@ def parse_args():
                         required=False,
                         type=str,
                         default='dataset_test')
+
+    parser.add_argument('--num-test-classes',
+                        help='Number of classes to take validation',
+                        required=False,
+                        type=str,
+                        default=5)
     return parser.parse_args()
 
 
@@ -46,15 +64,15 @@ if __name__ == "__main__":
     args = parse_args()
 
     print("Creating Training Dataset")
-    train = JsonCreator(args.train_dir, args.train_output)
+    train = JsonCreator(args.train_dir, args.train_output, args.num_train_classes)
     train.make()
 
     if args.val_dir:
         print("Creating Validation Dataset")
-        valid = JsonCreator(args.val_dir, args.val_output)
+        valid = JsonCreator(args.val_dir, args.val_output, args.num_val_classes)
         valid.make()
 
     if args.test_dir:
         print("Creating Test Dataset")
-        test = JsonCreator(args.test_dir, args.test_output)
+        test = JsonCreator(args.test_dir, args.test_output, args.num_test_classes)
         test.make()
